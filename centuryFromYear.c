e9f48e4bce6ca6752874631f71cfb76fe2c05eed
AhmedMostafa1318 <es-ahmed.ismail1318@alexu.edu.eg>
2018-06-22 04:02:27 +0200

    task added

---
+++ centuryFromYear.c
@@ -0,0 +1,16 @@
+int centuryFromYear(int year) 
+{
+    
+    for(int i=1;i<2005;i+=100)
+    {
+        if(year>=i && year<i+100)
+        {
+            return ((i+100)/100);
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
---
+++ checkPalindrome.c
@@ -0,0 +1,43 @@
+bool checkPalindrome(char * inputString) 
+{
+    int i=0,counter=0;
+    
+    while(inputString[counter]!='\0')
+    {
+        ++counter;//7
+    }
+  
+    for(i=0;i<=(counter/2);i++)//3
+    {
+        if(counter==1)
+        {
+            --counter;
+            break;
+        }
+        else if(inputString[i]==inputString[counter-1-i])
+        {
+            if(i==(counter/2))
+            {
+                break;
+            }
+            continue;
+        }
+        else
+        {
+            break;   
+        }
+    }
+    if(counter==0)
+    {
+        return true;
+    }
+    else if(i==(counter/2))
+    {
+        return true;
+    }
+    else
+    {
+        return false;    
+    }
+    
+}
