1f67a8781a5707ea4bf67375084fe4f6efa8e24f
AhmedMostafa1318 <es-ahmed.ismail1318@alexu.edu.eg>
2018-06-23 04:14:05 +0200

    task added

---
+++ adjacentElementsProduct.c
@@ -0,0 +1,19 @@
+int adjacentElementsProduct(arr_integer inputArray) 
+{
+    int i=0,max=0,temp=2;
+    int size=inputArray.size;
+    
+    max=inputArray.arr[i]*inputArray.arr[i+1];
+    
+    for(i=1;i<(size-1);i++)
+    {
+
+            temp=inputArray.arr[i]*inputArray.arr[i+1];
+            if(temp>max)
+            {
+                max=temp;
+            }
+            else{}
+    }
+    return max;     
+}
